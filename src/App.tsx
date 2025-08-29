3f75b58a44d13dae55d3173db3cb9783bcd49ef4
“malcolmmutare” <mutarem@gmail.com>
2025-08-30 00:02:52 +0200

    set-up: implemented core components for the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const familyTreeData = [
+     {
+    id: 1,
+    label: "Parent 1",
+    children: [
+      { id: 2, label: "Child 1" },
+      { id: 3, label: "Child 2" },
+      {
+        id: 4,
+        label: "Parent 2",
+        children: [
+          { id: 5, label: "Child 1" },
+          { id: 6, label: "Child 2" },
+        ],
+      },
+    ],
+  },
+  {
+    id: 7,
+    label: "Parent 3",
+    children: [
+      { id: 8, label: "Child 1" },
+      { id: 9, label: "Child 2" },
+    ],
+  },
+];
 
+const FamilyTree = ({data}) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    </div>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <div>
+      <h2>Soprano's Family Tree</h2>
+      <FamilyTree
+        data={familyTreeData}
+        />
+    </div>
+  )
+}
