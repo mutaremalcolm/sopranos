7f5bf067d8515cbf8d7eadb2c4f87bd79d015d68
“malcolmmutare” <mutarem@gmail.com>
2025-10-17 19:50:37 +0200

    refactor: refactor app to use pure javascript

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,52 @@
 import "./App.css";
 
-const familyTreeData = [
+const FamilyTreeData = [
      {
     id: 1,
-    label: "Parent 1",
+    name: "Parent 1",
     children: [
       { id: 2, label: "Child 1" },
       { id: 3, label: "Child 2" },
       {
         id: 4,
-        label: "Parent 2",
+        name: "Parent 2",
         children: [
-          { id: 5, label: "Child 1" },
-          { id: 6, label: "Child 2" },
+          { id: 5, name: "Child 1" },
+          { id: 6, name: "Child 2" },
         ],
       },
     ],
   },
   {
     id: 7,
-    label: "Parent 3",
+    name: "Parent 3",
     children: [
-      { id: 8, label: "Child 1" },
-      { id: 9, label: "Child 2" },
+      { id: 8, name: "Child 1" },
+      { id: 9, name: "Child 2" },
     ],
   },
 ];
 
 const FamilyTree = ({data}) => {
-  return (
+  return (  
     <div>
-      {data}
+      {data.map (node=> (
+         <div key="id"> 
+          <input type="checkbox" />
+        <span>{node.name}</span>
+        {node.children &&  <FamilyTreeData data={node.children} />} 
+        </div>
+        ))}   
     </div>
   )
 }
-
+  
 export default function App() {
   return (
     <div>
       <h2>Soprano's Family Tree</h2>
       <FamilyTree
-        data={familyTreeData}
+        data={FamilyTreeData}
         />
     </div>
   )
